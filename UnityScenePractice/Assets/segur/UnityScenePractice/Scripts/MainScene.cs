4f07fec7611cb159d4ebb1df572481005f07cb41
segur <segur.opus@gmail.com>
2019-12-02 20:13:18 +0900

    Added scripts

---
+++ UnityScenePractice/Assets/segur/UnityScenePractice/Scripts/MainScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour
+{
+    void Awake()
+    {
+        if (!SceneController.AlreadyLoadScene("Common"))
+        {
+            SceneManager.LoadScene("Common", LoadSceneMode.Additive);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            if (SceneController.AlreadyLoadScene("Main"))
+            {
+                SceneManager.UnloadSceneAsync("Main");
+            }
+            SceneManager.LoadScene("Title", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ UnityScenePractice/Assets/segur/UnityScenePractice/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneController
+{
+    /** 既にシーンが読み込まれているかどうか */
+    public static bool AlreadyLoadScene(string name)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
